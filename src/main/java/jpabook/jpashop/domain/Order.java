1d1230e70ae2115f9f4bf39f4b033dd9090333f4
injinjeong <snowwwwww@naver.com>
2022-01-01 00:40:51 +0900

    remote add

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -38,7 +38,7 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    //연관관계 매서드
+    //==연관관계 매서드==
     public void setMember(Member member){
         this.member = member;
         member.getOrders().add(this);
