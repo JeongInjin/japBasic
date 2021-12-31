5f9d2402265239763625feb4a3bba646105341c1
injinjeong <snowwwwww@naver.com>
2021-12-31 16:20:28 +0900

    p6spy libs add(sql logging)

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -20,6 +18,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    //@Rollback(value = false)
     public void testMember() throws Exception{
         //given
         Member member = new Member();
