29cdba3150f8605c6524fbc75e88d6e330bbcccd
injinjeong <snowwwwww@naver.com>
2021-12-31 23:33:11 +0900

    Address Entity default Construcor add

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    //jpa 스펙상 엔티티나 임베디드 타입('@Embeddable')은 자바 기본 생성자를 'public' or 'protected'로 살장헤야 한다.
+    //jpa가 이런 제약을 두는 이유는 JPA 구현 라이브러리가 객체를 생성할 때 리플렉션 같은 기술을 사용할 수 있도록 지원해야 하기 때문이다.
+    protected Address() {}
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
