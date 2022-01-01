dd178825a89dd3f9bc0de83c951fde9ce80fe757
injinjeong <snowwwwww@naver.com>
2022-01-01 12:09:30 +0900

    test case 추가 및 test application.yml 추가(메모리디비사용) 등--

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //MemberService 에서는 readOly 옵션이 더 많아서 기본을 true로 주고, join 메서드는 Transactional 을 추가해 주었다.
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    //중복회원 체크
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if(!findMembers.isEmpty()) throw new IllegalStateException("이미 존재하는 회원입니다.");
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    //회원 단건 조회
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+/*
+    JUnit5 부터는 @RunWith 가 아닌 Extension 이라는 일관된 방법을 사용한다.
+    @SpringBootTest 어노테이셔 안에 @ExtendWith(SpringExtension.class) 을 포함한다.
+ */
+//-----> @ExtendWith(SpringExtension.class)
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("jeong");
+
+        //when
+        Long saveId = memberService.join(member);
+        System.out.println("saveId : " + saveId);
+
+        //then
+        em.flush();
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member memberA = new Member();
+        memberA.setName("jeong");
+
+        Member memberB = new Member();
+        memberB.setName("jeong");
+
+        //when
+        memberService.join(memberA);
+
+        //then
+        IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> memberService.join(memberB));
+        //assertEquals("이미 존재하는 회원입니다.", thrown.getMessage());
+    }
+
+}
