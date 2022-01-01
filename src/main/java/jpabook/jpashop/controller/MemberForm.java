2b7edde668d13f372fe94287858dff3b9d6e9dc5
injinjeong <snowwwwww@naver.com>
2022-01-01 21:23:24 +0900

    error validation check for contoller ~ html

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -9,8 +9,9 @@ import javax.validation.constraints.NotEmpty;
 @Setter
 public class MemberForm {
 
-    @NotEmpty(message = "")
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
     private String name;
+
     private String city;
     private String street;
     private String zipcode;
