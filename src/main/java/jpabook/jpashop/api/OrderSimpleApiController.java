cb535c76beedafe0f88684411538fdda9a62f427
injinjeong <snowwwwww@naver.com>
2022-01-03 00:48:03 +0900

    desc add

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -85,7 +85,12 @@ public class OrderSimpleApiController {
     /*
      * 성능이 좋을지는 모르겠ㅇ나, 재활용성이 힘들다.
      * DTO 를 조회하였기 때문에 데이터 변경이 힘들다.
-     *
+     * ---
+     * 쿼리방식 선택 권장 순서
+     * 1.우선 엔티티를 DTO 로 변환하는 방법을 선택한다. -> V2
+     * 2.필요하면 페치 조인으로 성능을 최적화 한다. -> 대부분의 성능 이슈가 해결된다. -> V3
+     * 그래도 안되면 DTO 로 직접 조회하는 방법을 사용한다. -> V4
+     * 최후의 방법은 JPA가 제공하는 네이티브 SQL 이나 스프링 JDBC Template 를 사용하여 직접 SQL 을 직접 사용한다.
      * */
     @GetMapping("/api/v4/simple-orders")
     public List<OrderSimpleQueryDto> ordersV4() {
