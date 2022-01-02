165c183cd832332236340632b2363f8b3c368788
injinjeong <snowwwwww@naver.com>
2022-01-02 22:02:30 +0900

    회원 수정 API

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -17,7 +17,7 @@ public class MemberService {
 
     //회원가입
     @Transactional
-    public Long join(Member member){
+    public Long join(Member member) {
         validateDuplicateMember(member);
         memberRepository.save(member);
 
@@ -28,16 +28,22 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
 
-        if(!findMembers.isEmpty()) throw new IllegalStateException("이미 존재하는 회원입니다.");
+        if (!findMembers.isEmpty()) throw new IllegalStateException("이미 존재하는 회원입니다.");
     }
 
     //전체 회원 조회
-    public List<Member> findMembers(){
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
     //회원 단건 조회
-    public Member findOne(Long memberId){
+    public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
+    }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
     }
 }
